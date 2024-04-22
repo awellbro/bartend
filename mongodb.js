f7cc9d55f4b58d01a77c90c9975139e95c0d6d78
Aaron <wellbrockaaron@gmail.com>
2024-04-21 23:24:23 -0700

    linked mongoDb to mongodb.js, added html for user input and added some css for pretty

---
+++ mongodb.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require('mongodb');
+
+const uri = "mongodb+srv://awellbro:Awellbro@drinks.gg5mw3e.mongodb.net/";
+const client = new MongoClient(uri);
+
+const db = client.db('drinksDb');
+const collection = db.collection('drinksDb');
+
+async function insert(){
+    try {
+        //await client.connect();
+        //const first = await collection.findOne();
+        //console.log(first)
+        await collection.insertOne({
+            name: "Pina Colada",
+            ingredients: ["rum", "pineapple", "ice", "cream of coconut"],
+            instructions: "test4",
+        });
+ 
+    } catch(error){
+        console.log('error', error);
+    } finally {
+        await client.close();
+    }
+};
+
+insert();
